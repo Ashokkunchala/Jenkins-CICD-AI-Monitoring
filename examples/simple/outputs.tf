0d091108cb83654b72d6954efd3ea59edee902fa
Kunchala Ashok <93498307+Ashokkunchala@users.noreply.github.com>
2026-09-21 19:39:57 +0530

    feat(simple): add low-cost Jenkins AI monitoring lab

---
+++ examples/simple/outputs.tf
@@ -0,0 +1,4 @@
+output "jenkins_public_ip" { value = module.jenkins.jenkins_master_public_ip }
+output "jenkins_url" { value = "http://"+module.jenkins.jenkins_master_public_ip+":8080" }
+output "ai_webhook_url" { value = module.ai.webhook_url }
+output "ai_state_machine_arn" { value = module.ai.state_machine_arn }
