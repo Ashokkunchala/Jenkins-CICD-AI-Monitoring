5cd2cfffaf48b8247d08612e2cc737006c9985a5
Kunchala Ashok <93498307+Ashokkunchala@users.noreply.github.com>
2026-09-21 19:57:57 +0530

    fix(simple): correct Jenkins URL interpolation

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -55,6 +55,6 @@ module "ai" {
   extra_tags = { CostProfile = "simple-lab" }
 }
 
-output "jenkins_url" { value = "http://"+module.jenkins.jenkins_master_public_ip+":8080" }
+output "jenkins_url" { value = "http://${module.jenkins.jenkins_master_public_ip}:8080" }
 output "ai_webhook_url" { value = module.ai.webhook_url }
 output "ai_state_machine_arn" { value = module.ai.state_machine_arn }
